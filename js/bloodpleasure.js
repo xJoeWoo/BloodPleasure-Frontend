0f1416d7e3bc860bc632e38b1505c4a82c5ca9e2
Ng <xjoewoo@gmail.com>
2018-04-22 04:07:50 +0800

    Add chart, add logout, add subscribe to different hardware ids

--- js/bloodpleasure.js
+++ js/bloodpleasure.js
@@ -1,50 +1,31 @@
 mqttClient = null;
 
-function connectMqtt() {
-    mqttClient = new Paho.MQTT.Client(getMqttUrl(), Number(9003), "webpage_" + parseInt(Math.random() * 100, 10));
-    mqttClient.onConnectionLost = onMqttLost;
-    mqttClient.onMessageArrived = onMqttReceived;
-    mqttClient.connect({userName: getMqttUser(), password: getMqttPassword(), onSuccess: onMqttConnect});
+function hasAccountId() {
+    return getAccountId() != null && getAccountId() > 0
 }
 
-function hasAccountId() {
-    return localStorage.getItem("accountId") != null && localStorage.getItem("accountId") >= 0
+function clearAccountId() {
+    localStorage.removeItem("accountId");
+}
+
+function setAccountId(value) {
+    localStorage.setItem("accountId", value);
+}
+
+function getAccountId() {
+    return localStorage.getItem("accountId")
+}
+
+function jump(url) {
+    window.location.replace(url)
+}
+
+function jumpIndex() {
+    jump("index.html")
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function requestHardwareId(accountId) {
-    return $.get(getApiRootUrl() + "/hardware", {accountId: accountId})
-}
-
-function onMqttReceived(message) {
-    console.log("onMqttReceived:" + message.payloadString);
-    if (!isHardwareProvider()) {
-        showMonitorData(parseMessage(message.payloadString).hardwareValue.payload.data.value)
-    }
-}
-
-function hardwareValue(result, androidId) {
-
-    let message = makeMessage(result, androidId);
-
-    let value = message.hardwareValue.payload.data.value;
-
-    showMonitorData(value);
-
-    console.log(message);
-
-    let monitorCircular = getMonitorCircular();
-
-    publishMqttMessage(makeMqttMessage(message))
-
-}
-
-
-function showMonitorData(value) {
-    getMonitorData().innerText = value;
 }
 
 function parseHardwareResult(json) {
@@ -56,46 +37,17 @@ function parseMessage(message) {
 }
 
 function makeMessage(result, androidId) {
-    return {hardwareValue: parseHardwareResult(result), androidId: androidId};
+    return {hardwareValue: parseHardwareResult(result), hardwareId: androidId};
 }
 
-function makeMqttMessage(value) {
-    let mqttMessage = new Paho.MQTT.Message(JSON.stringify(value));
-    mqttMessage.destinationName = getTopic();
-    return mqttMessage;
-}
-
-function publishMqttMessage(value) {
-    if (mqttClient != null && mqttClient.isConnected()) {
-        mqttClient.send(value);
-    }
-}
-
-function getMonitorData() {
-    return document.getElementById("monitor-data");
-}
-
-function getMonitorCircular() {
-    return document.getElementById("monitor-circular");
-}
-
-function onMqttConnect() {
-    console.log("onMqttConnect");
-    if (!isHardwareProvider()) {
-        mqttClient.subscribe(getTopic());
-    }
-}
-
-function onMqttLost(responseObject) {
-    if (responseObject.errorCode !== 0) {
-        console.log("onMqttLost:" + responseObject.errorMessage);
+function getHardwareId() {
+    try {
+        return BloodPleasure.hardwareId()
+    } catch (e) {
+        return ""
     }
 }
 
 function isHardwareProvider() {
-    try {
-        return !!BloodPleasure.isHardwareProvider()
-    } catch (e) {
-        return false
-    }
+    return getHardwareId().length > 0
 }
